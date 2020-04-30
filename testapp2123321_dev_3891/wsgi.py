c32300486d9d601c0f8aa8f47a0195ced1fc9871
Crowdbotics <team@crowdbotics.com>
2020-04-30 10:42:17 +0000

    Initial commit.

---
+++ testapp2123321_dev_3891/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testapp2123321_dev_3891 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapp2123321_dev_3891.settings')
+
+application = get_wsgi_application()
